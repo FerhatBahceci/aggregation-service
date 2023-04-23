376121aef746f71a915ac02251d6764d26098bef
Ferhat Bahceci <ferhatbahceci@Ferhats-MacBook-Pro.local>
2023-04-24 01:55:35 +0200

    fix optional params left and we are done

--- src/main/java/com/fedex/aggregation/service/service/AggregationService.java
+++ src/main/java/com/fedex/aggregation/service/service/AggregationService.java
@@ -4,18 +4,17 @@ import com.fedex.aggregation.service.gateway.PricingGateway;
 import com.fedex.aggregation.service.gateway.ShipmentGateway;
 import com.fedex.aggregation.service.gateway.TrackGateway;
 import com.fedex.aggregation.service.model.AggregatedResponse;
-import com.fedex.aggregation.service.model.Pricing;
-import com.fedex.aggregation.service.model.Shipment;
 import com.fedex.aggregation.service.model.Track;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import java.util.List;
+import java.util.Map;
 
 import static com.fedex.aggregation.service.gateway.PricingClient.DEFAULT_PRICING;
 import static com.fedex.aggregation.service.gateway.ShipmentClient.DEFAULT_SHIPMENT;
 import static com.fedex.aggregation.service.gateway.TrackClient.DEFAULT_TRACK;
-import static java.util.Objects.nonNull;
+import static java.util.Objects.isNull;
 
 @Service
 public class AggregationService {
@@ -41,7 +40,16 @@ public class AggregationService {
                         .onErrorReturn(DEFAULT_TRACK)
                         .flatMap(t -> shipmentGateway.getShipment(shipments)
                                 .onErrorReturn(DEFAULT_SHIPMENT)
-                                .map(s -> new AggregatedResponse(p.getResponseMap(), t.getResponseMap(), s.getResponseMap())))
-                );
+                                .mapNotNull(s -> createAggregatedResponse(p.getResponseMap(), t.getResponseMap(), s.getResponseMap()))
+                        ));
+    }
+
+    private AggregatedResponse createAggregatedResponse(Map<String, Double> p, Map<Long, Track.Status> t, Map<Long, List<String>> s) {
+        Map<String, Double> pricing = !p.isEmpty() ? p : null;
+        Map<Long, Track.Status> track = !t.isEmpty() ? t : null;
+        Map<Long, List<String>> shipments = !s.isEmpty() ? s : null;
+        return isNull(pricing) && isNull(track) && isNull(shipments)
+                ? null
+                : new AggregatedResponse(pricing, track, shipments);
     }
 }
